96852e1013320e66603531bd25f1c6624a3cb3a4
Jhonatan Mattana <jhonatan-mattana@hotmail.com>
2017-03-16 12:59:12 -0300

    desenhando

--- src/Graphics/Design.java
+++ src/Graphics/Design.java
@@ -1,7 +1,10 @@
 package Graphics;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,9 +21,7 @@ public class Design extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Design frame = new Design(){
-						
-					};
+					Design frame = new Design();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -35,7 +36,22 @@ public class Design extends JFrame {
 	public Design() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
+		contentPane = new JPanel(){
+
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+
+				Graphics2D g2 = (Graphics2D)g;
+				//Height = Altura
+				//Width  = Largura
+				g2.setColor(Color.BLACK);
+				g2.fillRect(0 ,0 ,getWidth()/3, getHeight()/3);
+				g2.fillRect(getHeight() /3,getWidth()/3 ,getWidth(), getHeight());
+//				g2.fillRect(getWidth()/2, getHeight()/2, getWidth(), getHeight());
+			}
+			
+		};
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
